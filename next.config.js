81c45c021d370d7c485358dc75bce49ec3fb4ce8
Your Name <ishmamp3@gmail.com>
2025-02-24 16:24:15 -0800

    eslint disabled

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
+  module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
 }
 
 module.exports = nextConfig
