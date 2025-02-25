9b4da5ca0d828a1124d8e56fb12a6e47b6969498
Your Name <ishmamp3@gmail.com>
2025-02-24 16:47:17 -0800

    env type shi

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,7 +3,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON!;
 
 interface Message {
   id: number;
